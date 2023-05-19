0c73943f75077a92e7041c975820e7f53b580f22
Florian SIMUNEK <florian.simunek@gmail.com>
2023-05-19 23:15:55 +0200

    :sparkles: Add Thumb component

---
+++ src/components/Thumb/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Thumb.module.scss";
+
+export default function Thumb() {
+	return (
+		<div className={styles.thumbs}>
+			<p className={styles.title}>Titre de la location</p>
+		</div>
+	);
+}
