3e7dbe6ee7bd66f5d65994e859fdba3d951b6fb9
Florian SIMUNEK <florian.simunek@gmail.com>
2023-05-25 15:26:06 +0200

    :recycle: Refactor Lease component & page

--- src/components/LeaseInformations/index.jsx
+++ src/components/LeaseInformations/index.jsx
@@ -3,30 +3,7 @@ import TagsList from "../TagsList";
 import StarsList from "../StarsList";
 import DropDownsList from "../DropDownsList";
 
-const TAGS = [
-	{
-		name: "Cozy",
-	},
-	{
-		name: "Canal",
-	},
-	{
-		name: "Paris 10",
-	},
-];
-
-const DROP_DOWNS = [
-	{
-		title: "Description",
-		text: "Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied). ",
-	},
-	{
-		title: "Équipements",
-		lis: ["Climatisation", "Wi-Fi", "Cuisine", "Espace de travail", "Fer à repasser", "Sèche-cheveux", "Cintres"],
-	},
-];
-
-export default function LeaseInformations({ title, location, owner }) {
+export default function LeaseInformations({ title, location, owner, TAGS, DROP_DOWNS }) {
 	return (
 		<>
 			<div className={styles.informations}>
--- src/pages/Lease/index.jsx
+++ src/pages/Lease/index.jsx
@@ -2,6 +2,29 @@ import ImageSlider from "../../components/ImageSlider";
 import LeaseInformations from "../../components/LeaseInformations";
 
 export default function Lease() {
+	const TAGS = [
+		{
+			name: "Cozy",
+		},
+		{
+			name: "Canal",
+		},
+		{
+			name: "Paris 10",
+		},
+	];
+
+	const DROP_DOWNS = [
+		{
+			title: "Description",
+			text: "Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied). ",
+		},
+		{
+			title: "Équipements",
+			lis: ["Climatisation", "Wi-Fi", "Cuisine", "Espace de travail", "Fer à repasser", "Sèche-cheveux", "Cintres"],
+		},
+	];
+
 	return (
 		<main>
 			<ImageSlider />
@@ -9,6 +32,8 @@ export default function Lease() {
 				title="Cozy loft on the Canal Saint-Martin"
 				location="Paris, Île-de-France"
 				owner="Alexandre Dumas"
+				TAGS={TAGS}
+				DROP_DOWNS={DROP_DOWNS}
 			/>
 		</main>
 	);
