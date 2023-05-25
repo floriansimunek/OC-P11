2810edf24c5dc95ae297695666e72ee94f3d1d15
Florian SIMUNEK <florian.simunek@gmail.com>
2023-05-25 15:23:12 +0200

    :recycle: Refactor DropDownList component

--- src/components/DropDownsList/index.jsx
+++ src/components/DropDownsList/index.jsx
@@ -2,14 +2,12 @@ import styles from "./DropDownsList.module.scss";
 import DropDown from "../DropDown";
 
 export default function DropDownsList({ DropDowns, type }) {
+	const dropdownListClassName = `${styles.dropDownsList}
+	${type === "row" ? styles.flexDirection_row : styles.flexDirection_column}
+	${type === "row" ? styles.alignItems_start : styles.alignItems_center}`;
+
 	return (
-		<div
-			className={styles.dropDownsList}
-			style={{
-				flexDirection: type === "row" ? "row" : "column",
-				alignItems: type === "row" ? "flex-start" : "center",
-			}}
-		>
+		<div className={dropdownListClassName}>
 			{DropDowns.map((dropDown, k) => (
 				<DropDown title={dropDown.title} text={dropDown.text} lis={dropDown.lis} type={type} key={k} />
 			))}
