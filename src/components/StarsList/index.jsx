a566a82e7589da1769ef3b68067eb9e354695fb5
Florian SIMUNEK <florian.simunek@gmail.com>
2023-05-25 17:31:21 +0200

    :recycle: Refactor StarsList component

--- src/components/StarsList/index.jsx
+++ src/components/StarsList/index.jsx
@@ -2,15 +2,7 @@ import styles from "./StarsList.module.scss";
 import Star from "../Star";
 
 export default function StarsList({ stars }) {
-	let starsElements = [];
-
-	for (let i = 0; i < 5; i++) {
-		if (i < stars) {
-			starsElements.push(<Star color={"#ff6060"} key={i} />);
-		} else {
-			starsElements.push(<Star color={"#e3e3e3"} key={i} />);
-		}
-	}
+	const starsElements = Array.from({ length: 5 }, (_, i) => <Star color={i < stars ? "#ff6060" : "#e3e3e3"} key={i} />);
 
 	return <div className={styles.starsList}>{starsElements}</div>;
 }
