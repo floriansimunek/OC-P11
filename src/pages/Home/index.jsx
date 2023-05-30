94ff3f0aecc603175e590b55950b3f9d6a40a24a
Florian SIMUNEK <florian.simunek@gmail.com>
2023-05-30 22:26:33 +0200

    :recycle: Refactor Home page code

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,33 @@
+import React, { useEffect, useState } from "react";
 import "./Home.module.scss";
 import Banner from "../../components/Banner";
 import ThumbsList from "../../components/ThumbsList";
 import backgroundImage from "../../assets/img/home_banner.png";
 import Api from "../../utils/Api";
 
-let THUMBS = [];
-
 const API = new Api();
-const datas = await API.fetchData();
-
-datas.map((data) => {
-	THUMBS.push(data);
-});
 
 export default function Home() {
+	const [thumbs, setThumbs] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const datas = await API.fetchData();
+				setThumbs(datas);
+			} catch (error) {
+				console.error("Error fetching data : ", error);
+			}
+		}
+
+		fetchData();
+	}, []);
+
 	return (
 		<>
 			<main>
 				<Banner backgroundImage={backgroundImage} text="Chez vous, partout et ailleurs" />
-				<ThumbsList thumbs={THUMBS} />
+				<ThumbsList thumbs={thumbs} />
 			</main>
 		</>
 	);
