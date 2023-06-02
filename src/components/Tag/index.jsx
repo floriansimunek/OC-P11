93627fc54c13005cec63469bc2829c92c8b513e8
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-02 22:41:40 +0200

    :memo: Add documentation - Tag component

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,5 +1,10 @@
 import styles from "./Tags.module.scss";
 
+/**
+ * Tag Element with text
+ * @param {string} name Text of the Tag
+ * @returns {JSX.Element} Tag Element
+ */
 export default function Tag({ name }) {
 	return <div className={styles.tags}>{name}</div>;
 }
