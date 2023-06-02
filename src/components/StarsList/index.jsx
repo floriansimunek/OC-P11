61fbbcaaa52762a7accc1321ec973042e287a267
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-02 22:41:00 +0200

    :memo: Add documentation - StarsList component

--- src/components/StarsList/index.jsx
+++ src/components/StarsList/index.jsx
@@ -1,6 +1,11 @@
 import styles from "./StarsList.module.scss";
 import Star from "../Star";
 
+/**
+ * Rating of the Lease with colored Stars
+ * @param {Array<Star>} stars Red stars (X/5) : display rating
+ * @returns {JSX.Element} StarsList Element
+ */
 export default function StarsList({ stars }) {
 	const starsElements = Array.from({ length: 5 }, (_, i) => <Star color={i < stars ? "#ff6060" : "#e3e3e3"} key={i} />);
 
