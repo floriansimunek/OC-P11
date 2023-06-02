0ed9580560f4f8dfdf9fac0731a30743883aa587
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-02 22:31:51 +0200

    :memo: Add documentation - DropDownsList component

--- src/components/DropDownsList/index.jsx
+++ src/components/DropDownsList/index.jsx
@@ -1,6 +1,13 @@
 import styles from "./DropDownsList.module.scss";
 import DropDown from "../DropDown";
 
+/**
+ * List of multiple DropDown components
+ * @param {Array<DropDown>} DropDowns Array of DropDown component
+ * @param {string} [type] One or two DropDown per lines
+ * @returns {JSX.Element} DropDownsList Element
+ */
+
 export default function DropDownsList({ DropDowns, type }) {
 	const dropdownListClassName = `${styles.dropDownsList}
 	${type === "row" ? styles.flexDirection_row : styles.flexDirection_column}
