8d374d3457f98f6abf62fa66697f6741bdadedd8
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-02 22:42:29 +0200

    :memo: Add documentation - TagsList component

--- src/components/TagsList/index.jsx
+++ src/components/TagsList/index.jsx
@@ -1,6 +1,11 @@
 import styles from "./TagsList.module.scss";
 import Tag from "../Tag";
 
+/**
+ * Display Tags of the Lease
+ * @param {Array<Tag>} tags Tags of the lease
+ * @returns {JSX.Element} TagsList Element
+ */
 export default function TagsList({ tags }) {
 	return (
 		<ul className={styles.tagsList}>
