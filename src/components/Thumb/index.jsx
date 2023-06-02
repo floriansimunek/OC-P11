14c5c6732c374d031e1cc97f2e2e07becae4cec4
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-02 22:45:49 +0200

    :memo: Add documentation - Thumb component

--- src/components/Thumb/index.jsx
+++ src/components/Thumb/index.jsx
@@ -1,6 +1,13 @@
 import styles from "./Thumb.module.scss";
 import { Link } from "react-router-dom";
 
+/**
+ * Display Thumbnail of Lease with Title and Image
+ * @param {string} title Title of the Lease
+ * @param {string} id id of the Lease
+ * @param {string} backgroundImage Thumbnail image of the Lease
+ * @returns {JSX.Element} Thumb Element
+ */
 export default function Thumb({ title, id, backgroundImage }) {
 	const thumbStyle = {
 		"--thumb-background-image": `url(${backgroundImage})`,
