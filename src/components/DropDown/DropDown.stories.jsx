ba75f5e8e6bdffc9a155f2ae9c50d1c63c9cbc55
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-06 14:11:05 +0200

    :hammer: Add DropDown Story

---
+++ src/components/DropDown/DropDown.stories.jsx
@@ -0,0 +1,72 @@
+import DropDown from "./index";
+import { BrowserRouter } from "react-router-dom";
+
+export default {
+	title: "UI/DropDown",
+	component: DropDown,
+	tags: ["autodocs"],
+	parameters: {
+		layout: "fullscreen",
+	},
+	args: {
+		title: "Title",
+		text: "Description",
+		lis: ["List 1", "List 2"],
+		type: false,
+	},
+	argTypes: {
+		title: {
+			defaultValue: "",
+			description: "Title of the dropDown",
+			table: {
+				type: { summary: "string" },
+				defaultValue: { summary: "" },
+			},
+		},
+		text: {
+			defaultValue: "",
+			description: "Description",
+			table: {
+				type: { summary: "string" },
+				defaultValue: { summary: "" },
+			},
+		},
+		lis: {
+			defaultValue: "",
+			description: "List of items",
+			table: {
+				type: { summary: "object" },
+				defaultValue: { summary: "" },
+			},
+		},
+		type: {
+			defaultValue: false,
+			description: "One line dropDown or not",
+			table: {
+				type: { summary: "string" },
+				defaultValue: { summary: "row" },
+			},
+		},
+	},
+};
+
+export const Default = ({ title, text, lis, type }) => {
+	let dropDownType;
+
+	switch (type) {
+		case true:
+			dropDownType = "row";
+			break;
+		case false:
+			dropDownType = "";
+			break;
+		default:
+			dropDownType = "";
+	}
+
+	return (
+		<BrowserRouter>
+			<DropDown title={title} text={text} lis={lis} type={dropDownType} />
+		</BrowserRouter>
+	);
+};
