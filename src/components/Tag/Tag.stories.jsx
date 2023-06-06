03667b960da96ec4f122db11b878b68b0934be84
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-06 12:52:07 +0200

    :hammer: Add Tag Story

---
+++ src/components/Tag/Tag.stories.jsx
@@ -0,0 +1,25 @@
+import Tag from "./index";
+
+export default {
+	title: "UI/Tag",
+	component: Tag,
+	tags: ["autodocs"],
+	parameters: {
+		layout: "fullscreen",
+	},
+	args: {
+		name: "Test",
+	},
+	argTypes: {
+		name: {
+			defaultValue: "",
+			description: "Tag element with text",
+			table: {
+				type: { summary: "string" },
+				defaultValue: { summary: "" },
+			},
+		},
+	},
+};
+
+export const Default = ({ name }) => <Tag name={name} />;
