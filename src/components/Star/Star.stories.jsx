312ba1ca99590d92abb9a32580c56d0fc0fea15b
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-06 12:40:36 +0200

    :hammer: Add Star Story

---
+++ src/components/Star/Star.stories.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Star from "./index";
+
+export default {
+	title: "UI/Star",
+	component: Star,
+	tags: ["autodocs"],
+	parameters: {
+		layout: "fullscreen",
+	},
+	args: {
+		isActive: false,
+	},
+	argTypes: {
+		isActive: {
+			defaultValue: false,
+			description: "Indicates if the star is red or gray",
+			table: {
+				type: { summary: "string" },
+				defaultValue: { summary: "#e3e3e3" },
+			},
+		},
+	},
+};
+
+export const Default = ({ isActive }) => {
+	let color;
+
+	switch (isActive) {
+		case true:
+			color = "#ff6060";
+			break;
+		case false:
+			color = "#e3e3e3";
+			break;
+		default:
+			color = "#ff6060";
+	}
+
+	return <Star color={color} />;
+};
