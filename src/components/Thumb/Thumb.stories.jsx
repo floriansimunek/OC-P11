2656740d0e0d8f4938d6df1c06547d54fb903597
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-06 13:51:54 +0200

    :hammer: Add Thumb Story

---
+++ src/components/Thumb/Thumb.stories.jsx
@@ -0,0 +1,56 @@
+import Thumb from "./index";
+import { BrowserRouter } from "react-router-dom";
+
+export default {
+	title: "UI/Thumb",
+	component: Thumb,
+	tags: ["autodocs"],
+	parameters: {
+		layout: "centered",
+	},
+	args: {
+		id: "c67ab8a7",
+		title: "Appartement cosy",
+		cover: "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
+	},
+	argTypes: {
+		id: {
+			defaultValue: "",
+			description: "id of the lease",
+			table: {
+				type: { summary: "string" },
+				defaultValue: { summary: "" },
+			},
+		},
+		title: {
+			defaultValue: "",
+			description: "title of the lease",
+			table: {
+				type: { summary: "string" },
+				defaultValue: { summary: "" },
+			},
+		},
+		cover: {
+			defaultValue: "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
+			description: "Cover of the lease",
+			options: [
+				"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
+				"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-1.jpg",
+				"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-2-1.jpg",
+			],
+			control: {
+				type: "select",
+			},
+			table: {
+				type: { summary: "string" },
+				defaultValue: { summary: "" },
+			},
+		},
+	},
+};
+
+export const Default = ({ id, title, cover }) => (
+	<BrowserRouter>
+		<Thumb id={id} title={title} backgroundImage={cover} />
+	</BrowserRouter>
+);
