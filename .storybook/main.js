639327cb6ea8a182a87770833f62504434de7b40
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-06 12:03:00 +0200

    :bug: Fix storybook not working

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,7 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/preset-create-react-app",
-		"@storybook/addon-interactions",
-		"@storybook/preset-scss",
-	],
+	addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app", "@storybook/addon-interactions"],
 	framework: {
 		name: "@storybook/react-webpack5",
 		options: {},
