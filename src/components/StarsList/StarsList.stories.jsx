3e1f71d19c00b89483d23ec08e1f3e0e68676eed
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-06 12:46:31 +0200

    :hammer: Add StarsList Story

---
+++ src/components/StarsList/StarsList.stories.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import StarsList from "./index";
+
+export default {
+	title: "UI/StarsList",
+	component: StarsList,
+	tags: ["autodocs"],
+	parameters: {
+		layout: "fullscreen",
+	},
+	args: {
+		rating: 0,
+	},
+	argTypes: {
+		rating: {
+			defaultValue: 0,
+			description: "Indicates rating between 0 et 5",
+			control: {
+				type: "range",
+				min: 0,
+				max: 5,
+				step: 1,
+			},
+			table: {
+				type: { summary: "number[0, 5]" },
+				defaultValue: { summary: 0 },
+			},
+		},
+	},
+};
+
+export const Default = ({ rating }) => {
+	return <StarsList stars={rating} />;
+};
