961846983d15214258b9d79c458576439a7a1dfc
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-10 17:36:26 +0200

    :sparkles: Error 404 if leaseId isn't find

--- src/pages/Lease/index.jsx
+++ src/pages/Lease/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Error404 from "../404";
 import ImageSlider from "../../components/ImageSlider";
 import LeaseInformations from "../../components/LeaseInformations";
 import Api from "../../utils/Api";
@@ -9,12 +10,16 @@ const API = new Api();
 export default function Lease() {
 	const { id: leaseId } = useParams();
 	const [lease, setLease] = useState(null);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		async function fetchData() {
 			try {
 				const datas = await API.fetchData();
 				const selectedLease = datas.find((lease) => lease.id === leaseId);
+				if (!selectedLease) {
+					setError(true);
+				}
 				setLease(selectedLease);
 			} catch (error) {
 				console.error("Error fetching data:", error);
@@ -23,6 +28,10 @@ export default function Lease() {
 
 		fetchData();
 	}, [leaseId]);
+
+	if (error) {
+		return <Error404 />;
+	}
 
 	if (lease) {
 		const TAGS = lease.tags.map((tag) => ({ name: tag }));
